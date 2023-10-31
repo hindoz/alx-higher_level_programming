bf15e91466f16b78d04d4a701d24310bf9427bc3
hindoz <hindamir.99@gmail.com>
2023-10-30 18:02:40 -0700

    upload all files

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+print("First 3 letters: ", end = "")
+print(word[:3])
+print("Last 2 letters: ", end = "")
+print(word[-2:])
+print("Middle word: ", end = "")
+print(word[1:-1])
